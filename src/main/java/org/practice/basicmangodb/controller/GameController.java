459f38c49d0ffa4dcc94be7b86fa8fbe47d0c9fc
David Knight <disco.david.85@gmail.com>
2024-09-02 14:58:09 +0800

    CORS added.

--- src/main/java/org/practice/basicmangodb/controller/GameController.java
+++ src/main/java/org/practice/basicmangodb/controller/GameController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/gamecontroller")
+@CrossOrigin(origins = "http://localhost:5173")
 public class GameController {
 
     private final GameServiceI gameServiceI;
